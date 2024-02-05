4cb87a82d136dfd3ff48c86073223920b608cafc
Tom | VedianSOFT <149474235+tom-vediansoft@users.noreply.github.com>
2024-02-05 22:04:01 +0100

    Revert "Revert "feat(VD-60): Added title and slug component""

--- views/page/create.blade.php
+++ views/page/create.blade.php
@@ -1,5 +1,4 @@
 <x-vedian::cms-layout>
-
     {{-- <div class="relative">
         <div class="absolute inset-0 flex items-center" aria-hidden="true">
             <div class="w-full border-t border-gray-300"></div>
